96a253eb12fd40572ffb320bc049ec08b93b6e24
thanhdangquang <kwanthanh2101@gmail.com>
2024-10-15 01:05:49 +0700

    video component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MyComponent2 from "./components/NewComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      câm mồm vào
+      <MyComponent2></MyComponent2>
     </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>câm mồm vào</p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/components/NewComponent.js
@@ -0,0 +1,10 @@
+//class component
+import React from "react";
+
+class MyComponent2 extends React.Component {
+  //JSX
+  render() {
+    return <div> my component </div>;
+  }
+}
+export default MyComponent2;
