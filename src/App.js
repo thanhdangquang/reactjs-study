653ca288aa27e28d6fee912d5583c6531fa90aab
thanhdangquang <kwanthanh2101@gmail.com>
2024-10-17 23:00:31 +0700

    hocstate

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MyComponent2 from "./components/NewComponent";
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      câm mồm vào
-      <MyComponent2></MyComponent2>
-    </div>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>câm mồm vào</p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello Hehe
+        <MyComponent2></MyComponent2>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/NewComponent.js
+++ src/components/NewComponent.js
@@ -1,10 +1,22 @@
 //class component
 import React from "react";
-
+import "./NC.css";
 class MyComponent2 extends React.Component {
+  state = {
+    name: "dqt",
+    address: "hn",
+    age: 22,
+    sex: "gay",
+  };
+  //state sẽ là biến để lưu trữ dữ liệu,đơn giản là 1 object
+
   //JSX
   render() {
-    return <div> my component </div>;
+    return (
+      <div>
+        my name is {this.state.name} and im {this.state.sex}
+      </div>
+    );
   }
 }
 export default MyComponent2;
